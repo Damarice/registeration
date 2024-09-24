7d7b2737ac80e0d9de08f4fb24a1a314c2df6af5
Damarice <menganyidamarice@gmail.com>
2024-09-24 15:28:43 +0300

    'kk'

--- backend/routes/inviteRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const inviteController = require('../controllers/inviteController');
-
-// Route to register an email and get the share link
-router.post('/register', inviteController.registerEmail);
-
-module.exports = router;
